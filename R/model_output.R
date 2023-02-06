9d40e564407b4632d9e154b3a49443083447b39e
JSartini <55293050+JSartini@users.noreply.github.com>
2023-02-06 10:26:16 -0500

    Added skeleton implementations and tentative documentation for S3 methods coef(), vcov(), and print() meant for hglm objects

---
+++ R/model_output.R
@@ -0,0 +1,42 @@
+#' Extraction of hglm model coefficients
+#'
+#' @details Makes point estimates of hglm coefficients available.
+#'
+#' @param object, fitted model output of a hiper_glm() function call (class hglm)
+#' @param ..., potential additional arguments required for generic coef
+#'
+#' @return Named vector containing coefficient point estimates
+#'
+#' @export
+#'
+coef.hglm <- function(object, ...){
+  warning("Not yet implemented.")
+}
+
+#' Extraction of variance-covariance of hglm model coefficients
+#'
+#' @details Makes variance-covariance matrix of hglm coefficient estimates available
+#'
+#' @param object, fitted model output of a hiper_glm() function call (class hglm)
+#' @param ..., potential additional arguments required for generic vcov
+#'
+#' @return Labelled variance-covariance matrix
+#'
+#' @export
+vcov.hglm <- function(object, ...){
+  warning("Not yet implemented.")
+}
+
+#' Create and display curated output from hglm
+#'
+#' @details Generates informative output from fitted hglm
+#'
+#' @param x, fitted model output of a hiper_glm() function call (class hglm)
+#' @param ..., potential additional arguments required for generic print
+#'
+#' @return String displayed
+#'
+#' @export
+print.hglm <- function(x, ...){
+  cat("Text output of hiper_glm.")
+}
